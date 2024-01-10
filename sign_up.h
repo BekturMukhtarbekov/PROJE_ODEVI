c5a001eb3d182bd27ba0e67d9dd8607094ba9311
BekturMukhtarbekov <147035050+BekturMukhtarbekov@users.noreply.github.com>
2024-01-10 12:18:26 +0300

    Update sign_up.h

--- sign_up.h
+++ sign_up.h
@@ -15,7 +15,7 @@ void signup(){
 
     Kullanici newUser;
 
-    newUser.userId = nextUserId ;
+    newUser.userId = nextUserId;
 
     printf("_______________UYE OL________________\n\n");
     printf("Adiniz:");
@@ -35,18 +35,24 @@ void signup(){
     printf("Kullanici ID'niz: %d\n" , nextUserId);
     printf("_____________________________________\n");
 
+    // Verileri, turleriyle (Kullanici Id, Ad, Soyad vs.) beraber goruntuleyebilmek icin acilan bir dosya
     FILE *file = fopen("users.txt", "a");
-
     if (file == NULL) {
         printf("Dosya acilamadi.\n");
         return;
     }
-
-    fprintf(file, "Kullanici ID: %d\nAd: %s\nSoyad: %s\nBakiye: %.d\nSifre: %s\n\n",
+    fprintf(file, "Kullanici ID:\t%d\nAd:\t\t\t\t%s\nSoyad:\t\t\t%s\nBakiye:\t\t\t%d\nSifre:\t\t\t%s\n\n",
             nextUserId, newUser.name, newUser.surname, newUser.walletAmount, newUser.password);
     nextUserId++;
-
     fclose(file);
+
+    FILE *dosya = fopen("kullanici_islemleri.txt","a");
+    if (dosya == NULL) {
+        printf("Dosya acilamadi.\n");
+    }
+    fprintf(dosya, "%s %s kullanicisi %d ID numarasi ile uye oldu.\n\n", users[0].name, users[0].surname, users[0].userId);
+    fclose(dosya);
+
 }
 
 #endif //PROJEODEV_SIGN_UP_H
