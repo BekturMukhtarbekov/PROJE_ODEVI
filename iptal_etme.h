2138640d6b5012c34a4103d49ce82413d665ab73
BekturMukhtarbekov <147035050+BekturMukhtarbekov@users.noreply.github.com>
2024-01-10 12:20:55 +0300

    Update iptal_etme.h

--- iptal_etme.h
+++ iptal_etme.h
@@ -5,20 +5,29 @@
 #define PROJEODEV_IPTAL_ETME_H
 
 void iptalEt() {
+    int i = 0;
     int iptalID;
     printf("\nIptal etmek istediginiz ucusun numarasini girin:");
     scanf("%d", &iptalID);
 
-    for (int i = 0; i < 10; ++i) {
+    for (; i < 10; ++i) {
         if (rez[i] == iptalID) {
             printf("%d numarali ucus rezervasyonu iptal edildi.\n\n", rez[i]);
             rez[i] = 0; // İptal edilen rezervasyonun numarasını sıfırla
-            return; // İşlem tamamlandı, fonksiyondan çık
+            ucuslar[i].bosYer++;
+            users[0].walletAmount += ucuslar[i].ucret;
+            break;
         }
-        ucuslar[i].bosYer++;
     }
 
-    printf("Belirtilen numarada bir rezervasyon bulunamadi.\n\n");
+    // Kullanicinin ucusu iptal ettigini dosyaya kaydeder
+    FILE *dosya = fopen("kullanici_islemleri.txt","a");
+    if (dosya == NULL) {
+        printf("Dosya acilamadi.\n");
+        return;
+    }
+    fprintf(dosya, "%d ID numarali %s %s tarafindan %d no'lu ucus iptal edildi.\n\n", users[0].userId, users[0].name, users[0].surname, ucuslar[i].ucusId);
+    fclose(dosya);
 }
 
 #endif //PROJEODEV_IPTAL_ETME_H
