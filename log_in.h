ddd2ec24414e1433a6912d36465182aca48db1c2
BekturMukhtarbekov <147035050+BekturMukhtarbekov@users.noreply.github.com>
2024-01-10 12:19:00 +0300

    Update log_in.h

--- log_in.h
+++ log_in.h
@@ -22,10 +22,19 @@ int login() {
         if (users[i].userId == userId && strcmp(users[i].password, password) == 0) {
             printf("Giris basariyla yapildi\n");
             printf("Hos geldiniz, %s %s!\n\n", users[i].name, users[i].surname);
+
+            FILE *dosya = fopen("kullanici_islemleri.txt","a");
+            if (dosya == NULL) {
+                printf("Dosya acilamadi.\n");
+            }
+            fprintf(dosya, "%d ID numarali %s %s sayfasina giris yapti.\n\n", users[0].userId, users[0].name, users[0].surname);
+            fclose(dosya);
+
             return 1;
         }
     }
     return 0;
+
 }
 
 #endif //PROJEODEV_LOG_IN_H
